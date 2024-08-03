055a4239b5fc389e76e87dd5c57eb0a34b1cb4fd
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-08-04 00:00:50 +0900

    canvas generated

--- components/InputText.vue
+++ components/InputText.vue
@@ -1,12 +1,10 @@
 <template>
     <div>
-        <p>{{ title }} : {{ message }}</p>
-        <input v-model="message" placeholder="edit here" />
+        <p>{{ title }}</p>
+        <input/>
     </div>
 </template>
 <script setup lang="ts">
-const message = ref('')
-
 const props = defineProps({
     title: String,
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,33 @@
+<!-- <template>
+    <p>ここに画像いれるで</p>
+    <ImageUpload />
+    <InputText title="ポケモンの名前を入力"/>
+    <InputText title="性格"/>
+    <InputText title="出会った日"/>
+    <InputText title="睡眠時間"/>
+    <InputText title="出会った場所"/>
+    <InputText title="愛を語れ"/>
+    <canvas/>
+
+
+</template>
 <script setup>
 </script>
 
-<template>
-    <p>ここに画像いれるで</p>
-    <ImageUpload />
-</template>
 
 <style>
 p{
     top: 100px;   
 }
-</style>
+</style> -->
+
+<template>
+    <div>
+        <InputTest />
+    </div>
+</template>
+
+<script setup>
+import InputTest from '@/components/inputTest.vue';
+</script>
+  
