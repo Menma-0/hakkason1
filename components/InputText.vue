c8f786ac63e67fa05d7d9a215ead9d0dd07bf6be
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-08-03 16:51:53 +0900

    text input

---
+++ components/InputText.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>{{ title }} : {{ message }}</p>
+        <input v-model="message" placeholder="edit me" />
+    </div>
+</template>
+<script setup lang="ts">
+const message = ref('')
+
+const props = defineProps({
+    title: String,
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>index.vue</h1>
+    <InputText title="Name is"/>
+</template>
