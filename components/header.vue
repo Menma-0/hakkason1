b5b1a849eb41094f425490d153114953ea5e6179
Tsuji Yuta <shixiongtai70@gmail.com>
2024-08-03 14:37:24 +0900

    default2

---
+++ components/header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <h1 >マイポケ</h1>
+    </div>
+</template>
+
+<style scoped>
+.header{
+    margin: 0;
+    width: 100%;
+    height: 60px;
+    background: #FFF;
+    box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.25);
+    position: fixed;
+    display: flex;
+    align-items: center;
+    top: 0;
+    left: 0;
+}
+.header h1{
+    position: relative;
+    left: 10px;
+    margin: 0;
+}
+</style>
